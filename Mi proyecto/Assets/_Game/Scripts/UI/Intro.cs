3de47dfe2b0cf99b1f0e718511d99de0eef4fa48
Edwinst07 <edwinst98@gmail.com>
2023-07-23 00:43:17 -0500

    add audio at a intro
    add audio at a intro

---
+++ Mi proyecto/Assets/_Game/Scripts/UI/Intro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_audioSource.isPlaying == false)
+        {
+            _audioSource.Play();
+        }
+    }
+}
